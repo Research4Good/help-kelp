3e8344a8a35ce47f5a9987f49b99d39124e139ee
Tang <138358213+tcw1470@users.noreply.github.com>
2024-02-19 08:52:41 -0800

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -1,4 +1,41 @@
+import pandas as pd
+import rasterio
+import os
 import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset, DataLoader
+import flax
+import jax
+import jax.numpy as jnp
+from flax import linen as nn
+from flax.training import train_state
+from flax import struct, jax_utils
+from flax.training.common_utils import shard
+import optax
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import functools
+from typing import Any, List, Type, Union, Optional, Dict
+#import albumentations as albu
+
+from numpy import clip
+import plotly.express as px
+
+import random
+import shutil
+
+# gpu and tpu saving didn't work in same way
+try:
+    from flax.training import orbax_utils
+    from orbax.checkpoint import PyTreeCheckpointer
+    USE_ORBAX_WITH_FLAX = True
+except:
+    from orbax.checkpoint import Checkpointer, PyTreeCheckpointHandler, JsonCheckpointHandler, PyTreeCheckpointer
+    import nest_asyncio
+    nest_asyncio.apply()
+    USE_ORBAX_WITH_FLAX = False
+
 
 def checker( a, b, n=50 ):        
     assert a.shape == b.shape
