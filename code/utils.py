5ea838eaa649abec490e3f4eb9b77461b55899ec
Tang <138358213+tcw1470@users.noreply.github.com>
2024-02-19 09:04:52 -0800

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -25,6 +25,9 @@ import plotly.express as px
 import random
 import shutil
 
+
+
+
 # gpu and tpu saving didn't work in same way
 try:
     from flax.training import orbax_utils
@@ -36,6 +39,84 @@ except:
     nest_asyncio.apply()
     USE_ORBAX_WITH_FLAX = False
 
+
+def rescale( pixels, verbose=False ):
+    mean, std = pixels.mean(), pixels.std()+1e-10
+     
+    pixels = (pixels - mean) / std
+    
+    # clip pixel values to [-1,1]
+    # pixels = clip(pixels, -1.0, 1.0)
+    
+    # shift from [-1,1] to [0,1] with 0.5 mean
+    pixels = (pixels + np.abs( pixels.min()) ) 
+
+    pixels /= (pixels.max()+1e-10 )
+    
+    if verbose:
+        print('Mean: %.3f, Standard Deviation: %.3f' % (mean, std))        
+        print( pixels.min(), pixels.max() )
+    return pixels*255
+
+
+class Dataset(Dataset):
+    """
+    Args:
+        df - pandas.DataFrame with columns (image, mask, region)
+        transfrom - torchvision.transforms for image augmentations
+        inference - use the test mode
+        channels - list of channels that to be used
+    """
+    def __init__(self, df: Any, transform: Any = None, inference: bool = False, channels: List[int] = None):      
+        self.df = df
+        self.transform = transform
+        self.inference = inference
+        self.channels = channels
+            
+    def __len__(self):
+        return len(self.df)
+    
+    def _read_img(self, path: str, channels: List[int]):
+        path = img_dir + path + '_satellite.tif'
+        if channels:
+            img = rasterio.open(path).read(channels).transpose((1, 2, 0))
+        else:
+            img = rasterio.open(path).read().transpose((1, 2, 0))
+        # normalize values to [0, 1] interval
+        for d in range(img.shape[2] ):
+            img[:,:,d] = rescale(img[:,:,d]) #np.float32(img) / 65535
+        return img
+    
+    def _read_mask(self, path: str):
+        path = lab_dir + path + '_kelp.tif'
+        mask = rasterio.open(path).read().transpose((1, 2, 0))
+        mask = np.int32(mask)
+        return mask
+
+    def __getitem__(self, idx: int):
+        row = self.df.iloc[idx]
+        
+        #path = row['image']  # fire landsat
+        path = row['tile_id'] 
+        
+        image = self._read_img( path, self.channels)
+         
+        if self.inference:
+            return image
+        
+        mask = self._read_mask( path )
+        if self.transform:
+            sample = self.transform(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+
+        image= np.pad( image, ((1,1),(1,1),(0,0)) )
+        mask = np.pad( mask, ((1,1),(1,1),(0,0)) )
+
+        print( 'tileid:',path, 'AreaPercent:',mask.sum()/350/350 )
+        
+        return image, mask
+    
+    
 
 def checker( a, b, n=50 ):        
     assert a.shape == b.shape
